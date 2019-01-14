6b8b193f769db3494b221bc84e155bbfff9e7258
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-01-14 02:27:07 -0500

    Correcciones de variables y se añadieron comentarios finales

--- src/data.js
+++ src/data.js
@@ -66,8 +66,8 @@ const statFunction = (data, num) => {
 
 // Buscador
 const searchDataFunction = (data, dataSearch) => {
-  let dataCopy = [];
-  let arraySearch = [];
+  let dataCopy = []; // Copia de la data
+  let arraySearch = []; // Data en minúsculas
   let newArraySearch = [];
 
   for (let i = 0; i < data.length; i++)
@@ -75,8 +75,9 @@ const searchDataFunction = (data, dataSearch) => {
 
   for (let i = 0; i < dataCopy.length; i++) {
     arraySearch.push(dataCopy[i].name.toLowerCase());
-    if (arraySearch[i].indexOf(dataSearch.toLowerCase()) !== -1)
+    if (arraySearch[i].indexOf(dataSearch.toLowerCase()) !== -1) {
       newArraySearch.push(dataCopy[i]);
+    }  
   }
   return newArraySearch;
 };
